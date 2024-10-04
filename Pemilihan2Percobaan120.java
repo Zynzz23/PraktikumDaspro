10192c6d27ce8778e12a1ce70fcf74edf1118a55
sastra zaky <sastrazaky23@gmail.com>
2024-10-04 19:07:56 +0700

    Jobsheet6

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -5,19 +5,17 @@ public class Pemilihan2Percobaan120 {
     public static void main(String[] args) {
         Scanner input20 = new Scanner(System.in);
 
-        System.out.print("Masukkan tahun: ");
-        int tahun = input20.nextInt();
+            System.out.print("Masukkan tahun: ");
+            int tahun = input20.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");
-            } else if ((tahun % 400) == 0) {
-                System.out.println("Tahun Kabisat");
-            } else {
-                System.out.println("Bukan Tahun Kabisat");
-            }
-        } else {
-            System.out.println("Bukan Tahun Kabisat");
-        }
-    }
+            if ((tahun % 4) == 0) {
+                if ((tahun % 100) != 0)
+                    System.out.println("Tahun Kabisat");
+            } else 
+                System.out.println("Bukan Tahun Kabisat"); 
+        }                    
 }
+    
+    
+
+
